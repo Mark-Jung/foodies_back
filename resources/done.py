9209b05429fa165f186fa53aa09f8dbbcf80a4a8
Mark Jung <gujung2022@u.northwestern.edu>
2017-11-04 16:47:45 -0500

    finished the other end point

--- resources/done.py
+++ resources/done.py
@@ -5,14 +5,26 @@ import requests, json, random, ast
 
 class Done(Resource):
     parser = reqparse.RequestParser()
-    parser.add_argument('liked',
+    parser.add_argument('liked_photo_id',
         type=str,
         required=True,
-        help="This field cannot be left blank: liked"
+        help="This field cannot be left blank: liked_photo_id"
+    )
+    parser.add_argument('liked_yelp_id',
+        type=str,
+        required=True,
+        help="This field cannot be left blank: liked_yelp_id"
     )
 
     def post(self):
         data = Done.parser.parse_args()
-        result = data["liked"]
+        liked_yelp_id_list = data['liked_yelp_id'].split()
+        liked_photo_id_list = data['liked_photo_id'].split()
+        if len(liked_photo_id_list) != len(liked_yelp_id_list):
+            return {"message": "liked_photo_id and liked_yelp_id lengths must match!"}, 400
+        result = []
 
-        return {"message": result}, 200
+        for single_yelp_id, single_photo_id in zip(liked_yelp_id_list, liked_photo_id_list):
+            result.append(BusinessModel.find_by_yelp_id(single_yelp_id).json(single_photo_id))
+
+        return result, 200

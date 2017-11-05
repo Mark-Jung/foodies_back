12f6cbcb2a23baf88f5e6618cf5a1bc41a2370a1
Mark Jung <gujung2022@u.northwestern.edu>
2017-11-04 22:36:39 -0500

    index range error

--- resources/start.py
+++ resources/start.py
@@ -98,10 +98,8 @@ class Start(Resource):
                 business.save_to_db()
             for x in range(0, 3):
                 unrandomized.append(ast.literal_eval(business.photo_ids)[x])
-
         randomized = []
         for x in range(0, 30):
             photo_id = unrandomized[random.randint(0, 59)]
             randomized.append(PhotoModel.find_by_photo_id(photo_id).json())
-
         return randomized
